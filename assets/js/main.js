516f0dbd84c15fba8219734d638af5ca9ca2afc6
biazoners <beatrizberto@gmail.com>
2022-11-11 19:29:03 -0300

    Separação de arquivos CSS, início da configuração do consumo da API

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,18 @@
-console.log('Sucesso!');
+const offset = 0;
+const limit = 10;
+const url = 'https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}';
+
+//processamento assíncrono = uma hora a resposta chega! 
+fetch(url)
+    .then(function (response) {
+    console.log (response)
+    })
+    .catch(function (error){
+        console.error(error)
+    })
+    .finally(function () {
+        console.log ('Requisição concluída!')
+    })
+
+
+
