3003a5af077d965a784cd36e0dd331e53365a2d2
Beatriz <beatrizberto@gmail.com>
2023-03-26 21:19:32 -0300

    modelo, cores para os tipos

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,22 @@
 function convertPokemonToLi(pokemon) {
   return `
-      <li class="pokemon">
-      <span class="number">#${pokemon.order}</span>
+      <li class="pokemon ${pokemon.type}">
+      <span class="number">#${pokemon.number}</span>
       <span class="name">${pokemon.name}</span>
 
     <div class="detail">
       <ol class="types">
-        <li class="type">grass</li>
-        <li class="type">poison</li>            </ol>
-      <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="${pokemon.Name}">
-
-      
+        ${pokemon.types.map((type) => `<li class="type">${type}</li>`).join('')}
+      </ol>
+      <img src="${pokemon.photo}" alt="${pokemon.name}">      
 
     </div>
     
-  </li>`
-  
+  </li>`;
 }
-const pokemonList = document.getElementById('pokemonList')
+const pokemonList = document.getElementById("pokemonList");
 
 pokeApi.getPokemons().then((pokemons = []) => {
-  const newHtml = pokemons.map(convertPokemonToLi).join('')
-  pokemonList.innerHTML += newHtml
-
-
-})
+  const newHtml = pokemons.map(convertPokemonToLi).join("");
+  pokemonList.innerHTML += newHtml;
+});
---
+++ assets/js/pokemon-model.js
@@ -0,0 +1,10 @@
+class Pokemon {
+    number;
+    name;
+    type;
+    types = [];
+    photo;
+
+}
+
+/* https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png */
